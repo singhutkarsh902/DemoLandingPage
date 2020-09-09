1cc7c4aa35b4ecd6ea077eeabcae4954f03295aa
singhutkarsh902 <singhutkarsh902@gmail.com>
2020-09-09 23:37:50 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <h1>Services Page</h1>
+    );
+}
+
+export default Services;
