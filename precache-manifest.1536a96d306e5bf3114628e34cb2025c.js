59e83fbd5aa5cbdaac40a02637f9b1a0a39b7d11
singhutkarsh902 <singhutkarsh902@gmail.com>
2020-09-12 23:32:06 +0530

    Updates

---
+++ precache-manifest.1536a96d306e5bf3114628e34cb2025c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f72258939f26cfe036892de83cb9c6",
+    "url": "/DemoLandingPage/index.html"
+  },
+  {
+    "revision": "c4b1c9f55f24806a2b3f",
+    "url": "/DemoLandingPage/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3a0baee8768fde394901",
+    "url": "/DemoLandingPage/static/css/main.e6832ccc.chunk.css"
+  },
+  {
+    "revision": "c4b1c9f55f24806a2b3f",
+    "url": "/DemoLandingPage/static/js/2.6c94f291.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/DemoLandingPage/static/js/2.6c94f291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0baee8768fde394901",
+    "url": "/DemoLandingPage/static/js/main.6ae63f0a.chunk.js"
+  },
+  {
+    "revision": "316a70f558676264ab7f",
+    "url": "/DemoLandingPage/static/js/runtime-main.df6bcc3b.js"
+  },
+  {
+    "revision": "7f3f429bc52af7d770a5df013107f815",
+    "url": "/DemoLandingPage/static/media/about.7f3f429b.png"
+  },
+  {
+    "revision": "98560034b72804bba8a79efdd202f701",
+    "url": "/DemoLandingPage/static/media/android.98560034.jfif"
+  },
+  {
+    "revision": "2aeb12759ae67046f7199567f4e24fae",
+    "url": "/DemoLandingPage/static/media/home.2aeb1275.png"
+  },
+  {
+    "revision": "fa1d5e2acb8ad32a2f16663358f7adc6",
+    "url": "/DemoLandingPage/static/media/market.fa1d5e2a.jfif"
+  },
+  {
+    "revision": "aaf3691b201418b9f7db3fe46c7cd9c1",
+    "url": "/DemoLandingPage/static/media/mobile.aaf3691b.jfif"
+  },
+  {
+    "revision": "b9123f9570fa890b5359cae859a807f7",
+    "url": "/DemoLandingPage/static/media/software.b9123f95.jfif"
+  },
+  {
+    "revision": "624b163f0870def829a6375c9fc4f3fd",
+    "url": "/DemoLandingPage/static/media/team.624b163f.jfif"
+  },
+  {
+    "revision": "2a5b0be9f2f7485bcbca091082b8f77a",
+    "url": "/DemoLandingPage/static/media/web.2a5b0be9.jfif"
+  }
+]);
